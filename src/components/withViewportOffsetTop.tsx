d1e9894e4950afa906156007e3f4d4bbd59ee132
Cong Pham <ptcong411@gmail.com>
2024-02-27 01:31:49 +0700

    35380 virtual viewport on report screen - solution 01

--- src/components/withViewportOffsetTop.tsx
+++ src/components/withViewportOffsetTop.tsx
@@ -1,7 +1,9 @@
-import type {ComponentType, ForwardedRef, RefAttributes} from 'react';
-import React, {forwardRef, useEffect, useState} from 'react';
+import type { ComponentType, ForwardedRef, RefAttributes } from 'react';
+import React, { forwardRef, useEffect, useRef } from 'react';
 import getComponentDisplayName from '@libs/getComponentDisplayName';
 import addViewportResizeListener from '@libs/VisualViewport';
+import CONST from '@src/CONST';
+import useDebouncedState from '@hooks/useDebouncedState';
 
 type ViewportOffsetTopProps = {
     // viewportOffsetTop returns the offset of the top edge of the visual viewport from the
@@ -11,12 +13,26 @@ type ViewportOffsetTopProps = {
 
 export default function withViewportOffsetTop<TProps extends ViewportOffsetTopProps, TRef>(WrappedComponent: ComponentType<TProps & RefAttributes<TRef>>) {
     function WithViewportOffsetTop(props: Omit<TProps, keyof ViewportOffsetTopProps>, ref: ForwardedRef<TRef>) {
-        const [viewportOffsetTop, setViewportOffsetTop] = useState(0);
-
+        const [, viewportOffsetTop, setViewportOffsetTop] = useDebouncedState(0, CONST.TIMING.VIEWPORT_DEBOUNCE_TIME);
+        const initialHeight = useRef(window.visualViewport?.height ?? window.innerHeight).current;
+        const cachedDefaultOffsetTop = useRef<number>(0)
         useEffect(() => {
             const updateDimensions = (event: Event) => {
                 const targetOffsetTop = (event.target instanceof VisualViewport && event.target.offsetTop) || 0;
-                setViewportOffsetTop(targetOffsetTop);
+                if (window.visualViewport) {
+                    const adjustScrollY = Math.round(initialHeight - window.visualViewport.height);
+                    if (cachedDefaultOffsetTop.current === 0) {
+                        cachedDefaultOffsetTop.current = targetOffsetTop;
+                    }
+                    
+                    if (adjustScrollY > targetOffsetTop) {
+                        setViewportOffsetTop(adjustScrollY);
+                    } else if (targetOffsetTop !== 0 && adjustScrollY === targetOffsetTop) {
+                        setViewportOffsetTop(cachedDefaultOffsetTop.current);
+                    } else {
+                        setViewportOffsetTop(targetOffsetTop);
+                    }
+                }
             };
 
             const removeViewportResizeListener = addViewportResizeListener(updateDimensions);
@@ -24,7 +40,12 @@ export default function withViewportOffsetTop<TProps extends ViewportOffsetTopPr
             return () => {
                 removeViewportResizeListener();
             };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         }, []);
+
+        useEffect(() => {
+            window.scrollTo({ top: viewportOffsetTop });
+        }, [viewportOffsetTop])
 
         return (
             <WrappedComponent
