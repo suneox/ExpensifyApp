01bfd8a438710c4d8f64d66043cf68b720f68351
Cong Pham <ptcong411@gmail.com>
2024-02-28 00:30:52 +0700

    separate useViewportOffsetTop hook

---
+++ src/hooks/useViewportOffsetTop/index.ts
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
+import addViewportResizeListener from '@libs/VisualViewport';
+
+export default function useViewportOffsetTop(shouldAdjustScrollView = false): number {
+    const [viewportOffsetTop, setViewportOffsetTop] = useState(0);
+    const initialHeight = useRef(window.visualViewport?.height ?? window.innerHeight).current;
+    const cachedDefaultOffsetTop = useRef<number>(0);
+    useEffect(() => {
+        const updateDimensions = (event: Event) => {
+            const targetOffsetTop = (event.target instanceof VisualViewport && event.target.offsetTop) || 0;
+            if (window.visualViewport) {
+                const adjustScrollY = Math.round(initialHeight - window.visualViewport.height);
+                if (cachedDefaultOffsetTop.current === 0) {
+                    cachedDefaultOffsetTop.current = targetOffsetTop;
+                }
+
+                if (adjustScrollY > targetOffsetTop) {
+                    setViewportOffsetTop(adjustScrollY);
+                } else if (targetOffsetTop !== 0 && adjustScrollY === targetOffsetTop) {
+                    setViewportOffsetTop(cachedDefaultOffsetTop.current);
+                } else {
+                    setViewportOffsetTop(targetOffsetTop);
+                }
+            }
+        };
+
+        const removeViewportResizeListener = addViewportResizeListener(updateDimensions);
+
+        return () => {
+            removeViewportResizeListener();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!shouldAdjustScrollView) {
+            return;
+        }
+        window.scrollTo({top: viewportOffsetTop});
+    }, [shouldAdjustScrollView, viewportOffsetTop]);
+
+    return viewportOffsetTop;
+}
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1603,7 +1603,7 @@ const styles = (theme: ThemeColors) =>
 
         popoverInnerContainer: {
             paddingTop: 0, // adjusting this because the mobile modal adds additional padding that we don't need for our layout
-            maxHeight: '95%',
+            maxHeight: '99%',
         },
 
         menuItemTextContainer: {
