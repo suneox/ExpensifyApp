19636e49b3e026ee7d3d69b9aabf44451fc094ee
Fedi Rajhi <fedirjh@gmail.com>
2025-11-14 22:04:19 +0100

    feat: Update NewContactMethodConfirmMagicCodePage to use verifyAddSecondaryLoginCode and enhance error handling

--- src/pages/settings/Profile/Contacts/NewContactMethodConfirmMagicCodePage.tsx
+++ src/pages/settings/Profile/Contacts/NewContactMethodConfirmMagicCodePage.tsx
@@ -1,19 +1,16 @@
-import React, {useCallback, useEffect, useMemo} from 'react';
+import React from 'react';
 import ValidateCodeActionContent from '@components/ValidateCodeActionModal/ValidateCodeActionContent';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
-import usePrevious from '@hooks/usePrevious';
-import {addNewContactMethod as addNewContactMethodUser, clearContactMethod, clearUnvalidatedNewContactMethodAction, requestValidateCodeAction} from '@libs/actions/User';
+import {clearPendingContactActionErrors, clearUnvalidatedNewContactMethodAction, requestValidateCodeAction, verifyAddSecondaryLoginCode} from '@libs/actions/User';
 import {getLatestErrorField} from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
-import {addSMSDomainIfPhoneNumber} from '@libs/PhoneNumber';
 import {getContactMethod} from '@libs/UserUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
-import getDecodedContactMethodFromUriParam from './utils';
 
 type NewContactMethodConfirmMagicCodePageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.SETTINGS.PROFILE.NEW_CONTACT_METHOD_CONFIRM_MAGIC_CODE>;
 
@@ -23,29 +20,9 @@ function NewContactMethodConfirmMagicCodePage({route}: NewContactMethodConfirmMa
     const [account] = useOnyx(ONYXKEYS.ACCOUNT, {canBeMissing: false});
     const [session] = useOnyx(ONYXKEYS.SESSION, {canBeMissing: false});
     const contactMethod = getContactMethod(account?.primaryLogin, session?.email);
-    const newContactMethod = useMemo(() => getDecodedContactMethodFromUriParam(route.params.newContactMethod), [route.params.newContactMethod]);
 
     const [pendingContactAction] = useOnyx(ONYXKEYS.PENDING_CONTACT_ACTION, {canBeMissing: false});
-    const [loginList] = useOnyx(ONYXKEYS.LOGIN_LIST, {canBeMissing: true});
-
-    const prevPendingContactAction = usePrevious(pendingContactAction);
-    const loginData = loginList?.[pendingContactAction?.contactMethod ?? newContactMethod];
-    const validateLoginError = getLatestErrorField(loginData, 'addedLogin');
-
-    const addNewContactMethod = useCallback(
-        (magicCode: string) => {
-            addNewContactMethodUser(addSMSDomainIfPhoneNumber(newContactMethod), magicCode);
-        },
-        [newContactMethod],
-    );
-
-    useEffect(() => {
-        if (!pendingContactAction?.actionVerified) {
-            return;
-        }
-        clearUnvalidatedNewContactMethodAction();
-        Navigation.goBack(ROUTES.SETTINGS_CONTACT_METHOD_DETAILS.getRoute(addSMSDomainIfPhoneNumber(newContactMethod), navigateBackTo, true));
-    }, [navigateBackTo, newContactMethod, pendingContactAction, pendingContactAction?.actionVerified, prevPendingContactAction?.contactMethod]);
+    const validateCodeError = getLatestErrorField(pendingContactAction, 'addedLogin');
 
     return (
         <ValidateCodeActionContent
@@ -53,21 +30,14 @@ function NewContactMethodConfirmMagicCodePage({route}: NewContactMethodConfirmMa
             sendValidateCode={() => requestValidateCodeAction()}
             descriptionPrimary={translate('contacts.enterMagicCode', {contactMethod})}
             validateCodeActionErrorField="addedLogin"
-            validateError={validateLoginError}
-            handleSubmitForm={addNewContactMethod}
+            validateError={validateCodeError}
+            handleSubmitForm={verifyAddSecondaryLoginCode}
             clearError={() => {
-                if (!pendingContactAction?.contactMethod) {
-                    return;
-                }
-                clearContactMethod(newContactMethod);
+                clearPendingContactActionErrors();
             }}
             onClose={() => {
-                if (!pendingContactAction?.contactMethod) {
-                    return;
-                }
-                clearContactMethod(newContactMethod);
                 clearUnvalidatedNewContactMethodAction();
-                Navigation.goBack(ROUTES.SETTINGS_NEW_CONTACT_METHOD.getRoute(navigateBackTo));
+                Navigation.goBack(ROUTES.SETTINGS_CONTACT_METHODS.getRoute(navigateBackTo));
             }}
         />
     );
